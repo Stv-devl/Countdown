cd42b3e03f54972866d172f823a88cc4c6476707
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-08-07 10:33:46 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-// Créer la logique de compte à rebours
-
-// Créer un événement à la validation du form pour lancer le compte à rebours
-// variable
-
+//variables
 const form = document.querySelector("form");
 const choice = document.getElementById("choice");
 const countdownDisplay = document.getElementById("countdownDisplay");
-let totalSeconds = 0;
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  totalSeconds = choice.value * 60;
-  countDown();
-  choice.value = "";
-});
+let totalSeconds;
+let interval;
 
 function countDown() {
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = totalSeconds % 60;
+  let sec = seconds < 10 ? "0" + seconds : seconds; //for have 2 number on display
+  let min = minutes < 10 ? "0" + minutes : minutes; //for have 2 number on display
   if (totalSeconds > 0) {
-    totalSeconds = totalSeconds - 1;
-    countdownDisplay.innerHTML = `${minutes}.${seconds}`;
+    totalSeconds--; //-1 sec every second
+    countdownDisplay.textContent = `${min} : ${sec}`;
+  } else {
+    countdownDisplay.textContent = `It's finish`;
+    clearInterval(interval); //clear interval if its finish
   }
-  if (totalSeconds == 0) countdownDisplay.innerHTML = `write a number`;
   console.log(choice.value);
   console.log(minutes, seconds);
   console.log(totalSeconds);
 }
-setInterval(countDown, 1000);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (isNaN(choice.value)) {
+    //if its not a number
+    alert("Please write a number");
+  } else {
+    totalSeconds = choice.value * 60;
+    choice.value = ""; //clear form
+    clearInterval(interval); //clear interval when we click again
+    interval = setInterval(countDown, 1000); //repeat function every sec
+  }
+});
