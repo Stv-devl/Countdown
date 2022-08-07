1de5b9a8181f632c200001bad1dc26a7b98bf7cf
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-08-07 09:57:17 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Countdown
-Simple coundown app in javascript
+# From scratch - Create a simple countdown
+
+This is a solution for a simple countdown app
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Links
+
+- Solution URL: [Github code](https://github.com/Stv-devl/Todo-list-)
+- Live Site URL: [You can see here](https://stv-devl.github.io/Todo-list-/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- CSS
+- Javascript
+
+### What I learned
+
+Countdown in javascript
+
+## Author
+
+- Website - [Github](https://github.com/Stv-devl)
---
+++ index.js
@@ -0,0 +1,30 @@
+// Créer la logique de compte à rebours
+
+// Créer un événement à la validation du form pour lancer le compte à rebours
+// variable
+
+const form = document.querySelector("form");
+const choice = document.getElementById("choice");
+const countdownDisplay = document.getElementById("countdownDisplay");
+let totalSeconds = 0;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  totalSeconds = choice.value * 60;
+  countDown();
+  choice.value = "";
+});
+
+function countDown() {
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  if (totalSeconds > 0) {
+    totalSeconds = totalSeconds - 1;
+    countdownDisplay.innerHTML = `${minutes}.${seconds}`;
+  }
+  if (totalSeconds == 0) countdownDisplay.innerHTML = `finish`;
+  console.log(choice.value);
+  console.log(minutes, seconds);
+  console.log(totalSeconds);
+}
+setInterval(countDown, 1000);
